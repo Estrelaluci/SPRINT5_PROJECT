1912d8dc472b10ad3630b374ac0cbedf6f08e0e7
Your Name <you@example.com>
2024-12-03 12:41:59 -0300

    seventh issue

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-[st.header('Analisando anúncios de carros')]
+st.header('Analisando anúncios de carros')
 car_data = pd.read_csv('vehicles_us.csv') # lendo os dados 
 hist_button = st.button('Criar histograma') # criar um botão 
 if hist_button: # se o botão for clicado 
