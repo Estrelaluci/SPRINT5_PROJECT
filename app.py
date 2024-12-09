3c6b175bf5a755fd83d28f340e86f2565ac27122
Your Name <you@example.com>
2024-12-08 22:29:51 -0300

    nineth issue

--- app.py
+++ app.py
@@ -12,3 +12,13 @@ if hist_button: # se o botão for clicado
 # exibir um gráfico Plotly interativo 
     st.plotly_chart(fig, use_container_width=True) 
 
+
+car_data = pd.read_csv('vehicles_us.csv') # lendo os dados 
+scatter_button = st.button('Criar Dispersão') # criar um botão 
+if scatter_button: # se o botão for clicado 
+# escrever uma mensagem 
+st.write('Criando um gráfico de dispersão para o conjunto de dados de anúncios de vendas de carros') 
+# criar um grafico de dispersão 
+fig = px.scatter(car_data, x="odometer", y="price") # criar um gráfico de dispersão
+# exibir um gráfico Plotly interativo 
+st.plotly_chart(fig, use_container_width=True) 
